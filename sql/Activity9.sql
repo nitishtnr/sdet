c68ca639eae49980b29813d337b295b338acddc5
Nitish Raghava <50008156+nitishtnr@users.noreply.github.com>
2021-04-05 20:16:38 +0530

    Add files via upload

---
+++ sql/Activity9.sql
@@ -0,0 +1,107 @@
+REM   Script: Session 4
+REM   Test
+
+CREATE TABLE salesman (  
+    salesman_id int,  
+    salesman_name varchar2(32),  
+    salesman_city varchar2(32),  
+    commission int  
+);
+
+CREATE TABLE salesman (  
+    salesman_id int,  
+    salesman_name varchar2(32),  
+    salesman_city varchar2(32),  
+    commission int  
+);
+
+CREATE TABLE salesman (  
+    salesman_id int,  
+    salesman_name varchar2(32),  
+    salesman_city varchar2(32),  
+    commission int  
+);
+
+Describe salesman 
+
+
+select * from salesman;
+
+INSERT INTO salesman VALUES(5002, 'Nail Knite', 'Paris', 13);
+
+INSERT ALL  
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11)  
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14)  
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13)  
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12)  
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman;
+
+SELECT salesman_id, salesman_city FROM salesman;
+
+SELECT * FROM salesman WHERE salesman_city='Paris';
+
+CREATE TABLE salesman (  
+    salesman_id int,  
+    salesman_name varchar2(32),  
+    salesman_city varchar2(32),  
+    commission int  
+);
+
+CREATE TABLE salesman (  
+    salesman_id int,  
+    salesman_name varchar2(32),  
+    salesman_city varchar2(32),  
+    commission int  
+);
+
+CREATE TABLE salesman (  
+    salesman_id int,  
+    salesman_name varchar2(32),  
+    salesman_city varchar2(32),  
+    commission int  
+);
+
+Describe salesman 
+
+
+select * from salesman;
+
+INSERT INTO salesman VALUES(5002, 'Nail Knite', 'Paris', 13);
+
+INSERT ALL  
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11)  
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14)  
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13)  
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12)  
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman;
+
+SELECT salesman_id, salesman_city FROM salesman;
+
+SELECT * FROM salesman WHERE salesman_city='Paris';
+
+SELECT salesman_id, commission FROM salesman WHERE salesman_name='Paul Adam';
+
+Alter table salesman add grade int;
+
+select * from salesman;
+
+update table salesman set grade = 100;
+
+update salesman set grade = 100;
+
+select * from salesman;
+
+update salesman set grade = 200 where salesman_city='Rome';
+
+select * from salesman;
+
+update salesman set grade = 300 where salesman_name='Pit Alex';
+
+select * from salesman;
+
+update salesman set salesman_name = 'pierre'  where salesman_name='McLyon';
+
